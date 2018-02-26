183e2f6ce92213ccb4d20a9babaec5f8b8d5ae91
eMIKARp <emil.karpowicz@gmail.com>
2018-02-26 13:34:42 +0100

    Final window and its components look finished

--- src/pkg000_unitconverter/Main.java
+++ src/pkg000_unitconverter/Main.java
@@ -31,22 +31,29 @@ public class Main extends JFrame
     private JMenu mEdit = new JMenu("Edit"); // Edit menu
     private JMenu mHelp = new JMenu("Help"); // Help menu
     
-    private JLabel sCategory = new JLabel("Select Category");
-    private JLabel sUnit = new JLabel("Select Unit");
-    private JLabel iUnit = new JLabel("Input");
-    private JLabel oUnit = new JLabel("Output");
-    private JLabel sResult = new JLabel("Result");
+    private JLabel iUnit = new JLabel("                  Input Unit");
+    private JLabel oUnit = new JLabel("                  Output Unit");
+    private JLabel iResultUnit = new JLabel("Input Result Unit");
+    private JLabel oResultUnit = new JLabel("Output Result Unit");
     
     private JComboBox<String> sUnitCategory = new JComboBox<String>(); // unit category chooser
-    private JTextArea iUnitArea = new JTextArea();  // input unit chooser
-    private JTextArea oUnitArea = new JTextArea();  // output unit chooser
+    private JTextArea iUnitChooser = new JTextArea();  // input unit chooser
+    private JTextArea oUnitChooser = new JTextArea();  // output unit chooser
+    private JScrollPane iUnitChooser_Scroll = new JScrollPane(iUnitChooser,ScrollPaneConstants.VERTICAL_SCROLLBAR_ALWAYS, ScrollPaneConstants.HORIZONTAL_SCROLLBAR_AS_NEEDED);
+    private JScrollPane oUnitChooser_Scroll = new JScrollPane(oUnitChooser,ScrollPaneConstants.VERTICAL_SCROLLBAR_ALWAYS, ScrollPaneConstants.HORIZONTAL_SCROLLBAR_AS_NEEDED);
     
     private JTextField cArgument = new JTextField(); // conversion argument
     private JTextField cResult = new JTextField();   // conversion result
     
-    
+    private Category Length = new Category("Lenght", new String[]{"Meter","Kilometer","Centimeter","Millimeter","Nanometer","Mile","Yard","Foot","Inch"}); 
+            
     public Main()
     {
+        
+    /**
+     *  Building a main window and its components
+     */
+        
         this.setTitle("Unit Converter");
         this.setBounds((sWIDTH - 600)/ 2, (sHEIGHT-400) / 2, fWIDTH, fHEIGHT);
         this.setResizable(false);
@@ -65,24 +72,59 @@ public class Main extends JFrame
             mPanel.add(nPanel, BorderLayout.NORTH);
                 nPanel.setBorder(BorderFactory.createTitledBorder("Select Category"));
                 nPanel.setLayout(new BoxLayout(nPanel, BoxLayout.LINE_AXIS));
-                nPanel.add(Box.createHorizontalGlue());
+                nPanel.add(Box.createRigidArea(new Dimension(fWIDTH * 1/100, 0)));
                 nPanel.add(sUnitCategory);
-                    sUnitCategory.setPreferredSize(new Dimension(fWIDTH / 3,fHEIGHT / 14));
-                nPanel.add(Box.createRigidArea(new Dimension(fWIDTH - sUnitCategory.getSize().width -  220, 0)));
-                    this.addUnitCategory("");
-                    this.addUnitCategory("Lenght");
-                    this.addUnitCategory("Temperature");
-                    this.addUnitCategory("Area");
-                    this.addUnitCategory("Weight");
-                    this.addUnitCategory("Time");
-
+                    sUnitCategory.setPreferredSize(new Dimension(0,fHEIGHT / 14));
+                nPanel.add(Box.createRigidArea(new Dimension(fWIDTH * 55/100, 0)));
+            
             mPanel.add(cPanel, BorderLayout.CENTER);
                 cPanel.setBorder(BorderFactory.createTitledBorder("Select Unit"));
+                cPanel.setLayout(new BoxLayout(cPanel, BoxLayout.LINE_AXIS));
+                    cPanel.add(Box.createRigidArea(new Dimension(fWIDTH * 1/100,0)));
+                    Box cPanel_LeftVerticalPart = Box.createVerticalBox();
+                    cPanel.add(cPanel_LeftVerticalPart);
+                    cPanel_LeftVerticalPart.add(iUnit);
+                    cPanel_LeftVerticalPart.add(Box.createRigidArea(new Dimension(0,fHEIGHT * 1/100)));
+                    cPanel_LeftVerticalPart.add(iUnitChooser_Scroll);
+                    cPanel.add(Box.createRigidArea(new Dimension(fWIDTH * 1/10, 0)));
+                    Box cPanel_RightVerticalPart = Box.createVerticalBox();
+                    cPanel.add(cPanel_RightVerticalPart);
+                    cPanel_RightVerticalPart.add(oUnit);
+                    cPanel_RightVerticalPart.add(Box.createRigidArea(new Dimension(0,fHEIGHT * 1/100)));
+                    cPanel_RightVerticalPart.add(oUnitChooser_Scroll);
+                    cPanel.add(Box.createRigidArea(new Dimension(fWIDTH * 1/100,0)));
+                    
             mPanel.add(sPanel, BorderLayout.SOUTH);
+            
                 sPanel.setBorder(BorderFactory.createTitledBorder("Conversion results"));
-            
+                sPanel.setLayout(new BoxLayout(sPanel, BoxLayout.LINE_AXIS));
+                sPanel.add(Box.createRigidArea(new Dimension(fWIDTH * 1/100,0)));
+                Box sPanel_LeftVerticalPart = Box.createVerticalBox();
+                sPanel.add(sPanel_LeftVerticalPart);
+                cArgument.setPreferredSize(new Dimension(0, fHEIGHT/ 14));
+                sPanel_LeftVerticalPart.add(cArgument);
+                sPanel_LeftVerticalPart.add(Box.createRigidArea(new Dimension(0,fHEIGHT * 1/100)));
+                sPanel_LeftVerticalPart.add(iResultUnit);
+                sPanel.add(Box.createRigidArea(new Dimension(fWIDTH * 1/10, 0)));
+                Box sPanel_RightVerticalPart = Box.createVerticalBox();
+                sPanel.add(sPanel_RightVerticalPart);
+                cResult.setPreferredSize(new Dimension(0, fHEIGHT / 14));
+                sPanel_RightVerticalPart.add(cResult);
+                sPanel_RightVerticalPart.add(Box.createRigidArea(new Dimension(0,fHEIGHT * 1/100)));
+                sPanel_RightVerticalPart.add(oResultUnit);
+                sPanel.add(Box.createRigidArea(new Dimension(fWIDTH * 1/100,0)));
 
-            
+    /**
+     *  Building a main window and its components
+     */
+                
+//        this.addUnitCategory("");
+//        this.addUnitCategory("Lenght");
+//        this.addUnitCategory("Temperature");
+//        this.addUnitCategory("Area");
+//        this.addUnitCategory("Weight");
+//        this.addUnitCategory("Time");
+
     }
     
     public static void main(String[] args) 
@@ -90,8 +132,32 @@ public class Main extends JFrame
         new Main().setVisible(true);
     }
     
-    public void addUnitCategory(String cName)
+}
+
+class Category
+{
+    String cName;
+    String[] cUnits;
+    
+    public Category(String cName, String[] cUnits)
     {
-        sUnitCategory.addItem(cName);
+        this.cName = cName;
+        this.cUnits = cUnits;
     }
+    
+    public String getName()
+    {
+        return cName;
+    }
+    
+    public void getUnits()
+    {
+        for (int i = 0; i < cUnits.length; i++)
+            System.out.println(cUnits[i]);
+    }
+}
+
+class Unit
+{
+    String uName;
 }
